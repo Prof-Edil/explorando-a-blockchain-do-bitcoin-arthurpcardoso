e78f04ca1853bfd02ca4ceee529145bbba37920c
arthurpcardoso <arthurpcardoso13@gmail.com>
2025-01-27 22:06:49 -0300

    fix path

--- submission/006.sh
+++ submission/006.sh
@@ -1,2 +1,2 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
-python q6_v2.py
+python scripts/q6_v2.py
--- submission/007.sh
+++ submission/007.sh
@@ -1,2 +1,2 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
-python q7.py
+python scripts/q7.py
